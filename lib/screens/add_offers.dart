bc8df100b0405898d2e89d7ebcbac5ca33696593
akshay.k <akshaykc222@gmail.com>
2022-03-07 15:31:11 +0530

    notification added

--- lib/screens/add_offers.dart
+++ lib/screens/add_offers.dart
@@ -3,7 +3,9 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Offers extends StatefulWidget {
   const Offers({Key? key}) : super(key: key);
@@ -26,7 +28,7 @@ class _OffersState extends State<Offers> {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text('Uploading .....')));
       await FirebaseStorage.instance
-          .ref('uploads/$fileName.png')
+          .ref('uploads/$fileName')
           .putData(fileBytes!)
           .then((p0) async {
         final imgUrl = await p0.ref.getDownloadURL();
@@ -45,11 +47,65 @@ class _OffersState extends State<Offers> {
         .then((value) {
       setState(() {
         if (value.docs.first.data() != null &&
-            value.docs.first.data()!.containsKey('offers')) {
+            value.docs.first.data().containsKey('offers')) {
           image = value.docs.first.get('offers');
         }
       });
     });
+  }
+
+  getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return snapshot.data?.docs
+        .map((doc) => SizedBox(
+              width: double.infinity,
+              height: 200,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // SizedBox(
+                    //   width: 200,
+                    //   child: CachedNetworkImage(
+                    //     imageUrl: doc['offers'],
+                    //   ),
+                    // ),
+                    RichText(
+                        text: TextSpan(children: [
+                      const TextSpan(
+                          style: TextStyle(color: Colors.blue),
+                          text: "To View "),
+                      TextSpan(
+                          style: const TextStyle(color: Colors.blue),
+                          text: "Click here",
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () async {
+                              var url = doc['offers'];
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            }),
+                    ])),
+                    InkWell(
+                        onTap: () {
+                          FirebaseFirestore.instance
+                              .collection('offers')
+                              .doc(doc.id)
+                              .delete();
+                        },
+                        child: const Icon(
+                          Icons.delete,
+                          size: 30,
+                        ))
+                  ],
+                ),
+              ),
+            ))
+        .toList();
   }
 
   @override
@@ -73,7 +129,20 @@ class _OffersState extends State<Offers> {
                 Icons.add,
                 size: 50,
               )),
-          const Text('Upload file')
+          const Text('Upload file'),
+          StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('offers').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              return !snapshot.hasData
+                  ? const Center(
+                      child: Text('no offers'),
+                    )
+                  : ListView(
+                      shrinkWrap: true,
+                      children: getExpenseItems(snapshot),
+                    );
+            },
+          )
         ],
       ),
     );
